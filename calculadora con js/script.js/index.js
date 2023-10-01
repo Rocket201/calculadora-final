444d464875ecb6de195a7aaaa66f8c83b553156f
Alejandro Corroto <145266090+Rocket201@users.noreply.github.com>
2023-10-01 19:19:04 +0200

    Add files via upload
    tengo que añadir botones

---
+++ calculadora con js/script.js/index.js
@@ -0,0 +1,202 @@
+class Calculator {
+    constructor(operand1Element, operand2Element) {
+        this.operand1Element = operand1Element;
+        this.operand2Element = operand2Element;
+        this.clear();
+    }
+
+    clear() {
+        this.operand1 = 0;
+        this.operand2 = 0;
+        this.operator = '';
+        this.updateUI();
+    }
+
+    updateUI() {
+        this.operand1Element.innerHTML = this.operand1 + this.operator;
+        this.operand2Element.innerHTML = this.operand2;
+    }
+
+    appendNumber(number) {
+        if (number === "." && this.operand2.includes('.')) return;
+        this.operand2 = this.operand2 === 0 ? number : this.operand2.toString() + number;
+
+        this.updateUI();
+    }
+
+    delete() {
+        if (this.operand2 === 0) {
+            this.operand2 = +this.operand2.toString().slice(0, -1);
+            this.updateUI();
+        }
+    }
+
+    operation(operator) {
+        if (this.operator) {
+            this.calc();
+        }
+        this.operator = operator;
+        this.operand1 = +this.operand2 === 0 ? this.operand1 : this.operand2;
+        this.operand2 = 0;
+        this.updateUI();
+    }
+
+    calc() {
+        switch (this.operator) {
+            case "+":
+                this.operand1 = this.operand1 + this.operand2;
+                break;
+            case "-":
+                this.operand1 = this.operand1 - this.operand2;
+                break;
+            case "*":
+                this.operand1 = this.operand1 * this.operand2;
+                break;
+            case "/":
+                this.operand1 = this.operand1 / this.operand2;
+                break;
+        }
+        this.operator = "";
+        this.operand2 = 0;
+        this.updateUI();
+    }
+}
+
+const operand1Element = document.querySelector("[data-operand-1]");
+const operand2Element = document.querySelector("[data-operand-2]");
+const clearButton = document.querySelector("[data-clear]");
+const numberButtons = document.querySelectorAll("[data-number]");
+const deleteButton = document.querySelector("[data-delete]");
+const operationButtons = document.querySelectorAll("[data-operation]");
+const audioEquals = document.getElementById('audio'); 
+const equalsButtons = document.querySelector("[data-equals]");
+
+const audio = new Audio("../audio/musica.mp3");
+
+
+
+const calculator = new Calculator(operand1Element, operand2Element);
+
+
+
+clearButton.addEventListener("click", () => {
+    calculator.clear();
+});
+
+numberButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        calculator.appendNumber(button.innerHTML);
+    });
+});
+
+deleteButton.addEventListener("click", () => {
+    calculator.delete();
+});
+
+operationButtons.forEach(button => {
+    button.addEventListener("click", () => {
+        calculator.operation(button.innerHTML);
+    });
+});
+
+equalsButton.addEventListener("click", () => {
+    calculator.calc();
+    audio.play();
+  
+});
+
+
+
+/*class Calculator{
+    constructor(operand1Element, operand2Element){
+
+        this.operand1Element=operand1Element;
+        this.operand2Element= operand2Element;
+        this.clear();
+    }
+      clear(){
+        this.operand1=0;
+        this.operand2=0;
+        this.operator = '';
+        this.updateUI();
+       
+      }
+      updateUI(){
+        this.operand1Element.innerHTML = this.operand1 + this.operator;
+        this.operand2Element.innerHTML = this.operand2 ;
+      }
+    appendNumber(number){
+    if(number=== "." && this.operand2.includes('.')) return;
+    this.operand2=this.operand2 === 0 ? number :this.operand2.toString() + number;    
+    
+    this.updateUI();
+    }
+
+    delete(){
+        if(this.operand2 ===0 )
+        this.operand2 = +this.operand2.toString().slice(0,-1)
+        this.updateUI();
+    }
+
+    operation(operator){
+        if(this.operator){
+            this.calc();
+        }
+        this.operator = operator;
+        this.operand1 = +this.operand2 === 0 ? this.operand1 : this.operand2;
+        this.operand2 = 0;
+        this.updateUI();
+    }
+    calc(){
+        switch(this.operator){
+            case "+":
+                this.operand1 = this.operand1 + this.operand2;
+            break;
+            case "-":
+                this.operand1 = this.operand1 - this.operand2;
+            break;
+            case "*":
+                this.operand1 = this.operand1 * this.operand2;
+            break;
+            case "/":
+                this.operand1 = this.operand1 / this.operand2;
+            break;
+        }
+        this.operator = "";
+        this.operand2 = 0;
+        this.updateUI();
+    }
+}
+
+const operand1Element = document.querySelector("[data-operand-1]");
+const operand2Element = document.querySelector("[data-operand-2]");
+const clearButton = document.querySelector("[data-clear]");
+const numberButtons = documet.querySelectorAll("[data-number]");
+const deleteButton = document.querySelector("[data-delete]");
+const operationButtons = document.querySelectorAll("[data-operation]");
+const equalsButtons = document.querySelector("[data-equals]");
+
+
+const Calculator = new Calculator(operand1Element, operand2Element);
+
+clearButton.addEventListener("click", ()=>{
+    Calculator.clear();
+})
+
+numberButtons.forEach(button => {
+    button.addEventListener('click',()=>{
+        Calculator.appendNumber(button.innerHTML);
+    })
+});
+deleteButton.addEventListener('click', ()=>{
+    Calculator.delete();
+});
+
+ operationButtons.forEach(button =>{
+    button.addEventListener("click", ()=>{
+        Calculator.operation(button.innerHTML);
+    });
+ });
+equalsButtons.addEventListener("click", ()=>{
+    Calculator.calc();
+});*/
